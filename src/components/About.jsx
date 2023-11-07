fd8155e060e729ba78154692c46308423317dddd
Ritik pandey <kushagarakp10@gmail.com>
2023-11-07 16:14:42 +0530

    changing text3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,11 @@ const About = () => {
             <section  id='about'>
 <div className='bg-blue-400 shadow-lg mt-10 mx-auto w-full rounded-md flex-col gap-2'>
 
-                <h1 className='text-2xl  text-center pt-4'>Hii,I'm Ritik Pandey.Nice to meet you.</h1>
-                <h3 className='mt-4 text-center text-slate-100  text-xl'>
+                <h1 className='text-2xl  text-center pt-4'>Hii,I'm Ritik Pandey
+               </h1>
+                <h1 className='text-2xl  text-center '>
+                Nice to meet you.</h1>
+                <h3 className='mt-1 text-center text-slate-100  text-xl'>
                     Currently a student,Persuing Diploma in Information Technology  at Gp kashipiur,Uttarakhand.
                 </h3>
                 <h3 className='text-center  text-slate-100  text-xl'>
@@ -17,7 +20,7 @@ const About = () => {
                 I'm a firm believer in continuous learning,
                 </h3>
                 <h3  className='text-center  text-slate-100  text-xl'>
-                 and I always strive to stay at the forefront of web development trends.
+                 and I always strive to stay at the forefront of web development trends.
                 </h3>
                 <h3 className='text-center  text-slate-100 text-xl pb-4'>
                     Loves to code(obviously),exlporing new places (a lot sometimes),being creative.
