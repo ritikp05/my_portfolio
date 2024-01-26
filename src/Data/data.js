99afba09760901c8cb7230d89873d8050d28a10d
Ritik pandey <kushagarakp10@gmail.com>
2024-01-26 23:46:02 +0530

    added new projects

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,19 +1,39 @@
 export const data = [
-    {
-        id: 1,
-        name: "Rest Countries",
-        Tech: [" Reactjs ", " Tailwind CSS ", " Material UI ","Rest Countries API"],
-        Desc: "Discover countries easily with Rest Country App.Search, filter, and explore detailed info.",
-        github: "https://github.com/ritikp05/restcountries",
-        live: "https://restcountries00.netlify.app/"
-    },{
-    id: 2,
-        name: "Notes App",
-        Tech: [" Reactjs ","Redux", " SCSS ", "Material UI ",],
-        Desc: "NoteKeeper is a CRUD based note taking app with search functionality.  All data is stored in local storage. ",
-        github: "https://github.com/ritikp05/notes-app",
-        live: "https://noteskeeper01.vercel.app/"
-    },
-   
+  {
+    id: 1,
+    name: "E store",
+    Tech: [
+      " Reactjs ",
+      " Tailwind CSS ",
+      " Material UI ",
+      "Firebase",
+      "Fake store Api",
+    ],
+    Desc: "A E commerce web application developed using Fake store Api.",
+    github: "https://github.com/ritikp05/Ecommerce",
+    live: "https://e-store001.netlify.app/",
+  },
 
+  {
+    id: 2,
+    name: "VisionTube",
+    Tech: [
+      " Reactjs ",
+      "Redux",
+      "Tailwind CSS ",
+      "Material UI ",
+      "Rapid Api",
+    ],
+    Desc: "A video streaming web application which includes wide range of categories.",
+    github: "https://github.com/ritikp05/VisionTube",
+    live: "https://vision-tube-sandy.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Notes App",
+    Tech: [" Reactjs ", "Redux", " SCSS ", "Material UI "],
+    Desc: "NoteKeeper is a CRUD based note taking app with search functionality.  All data is stored in local storage. ",
+    github: "https://github.com/ritikp05/notes-app",
+    live: "https://noteskeeper01.vercel.app/",
+  },
 ];
